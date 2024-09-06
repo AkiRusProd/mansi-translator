903d944fe01df1fa2d8d62f1fcb89a0972881259
Rustam Akimov <raki@1c.ru>
2024-09-06 19:23:37 +0300

    minor changes (add raw tokenizer train)

---
+++ models/scripts/train_tokenizer.py
@@ -0,0 +1,110 @@
+from collections import Counter
+import sentencepiece as spm
+from sentencepiece import sentencepiece_model_pb2 as sp_pb2_model
+# At this step, the code may throw an error about protobuf. Do as it tells.
+from transformers import NllbTokenizer, AutoModelForSeq2SeqLM
+from tqdm import tqdm
+import pandas as pd
+
+from dataset import preproc
+ 
+# TODO: REFACTOR THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+df_train_mansi = pd.read_csv("data/cleared_v2.csv")
+
+df_train_mansi = df_train_mansi.rename(columns={ # TODO: This is bulshit, remove it
+    'target': 'mansi',
+    'source': 'ru'
+})
+
+ 
+print("Creating corpus and counting chars in it")
+# creating corpus
+all_texts = df_train_mansi["mansi"].dropna().tolist()
+all_text_normalized = [preproc(t) for t in tqdm(all_texts)]
+ 
+# counting characters
+chars_cnt = Counter(c for t in all_text_normalized for c in t)
+required_chars = ''.join([
+    k for k, v in chars_cnt.most_common() 
+    if v >= 3 and k not in ' '
+])
+ 
+# creating text-file with all texts
+all_texts_file = 'mansi_texts_plain.txt'
+SPM_PREFIX = 'spm_mansi_16k'
+with open(all_texts_file, 'w') as f:
+    for i, text in enumerate(all_texts):
+        print(text, file=f)
+ 
+# tokenizer train
+print("Tokenizer training")
+spm.SentencePieceTrainer.train(
+    input=all_texts_file,
+    model_prefix=SPM_PREFIX,
+    vocab_size=2**14,  # 16K
+    character_coverage = 1,
+    num_threads=16,
+    train_extremely_large_corpus=False,
+    add_dummy_prefix=False,
+    max_sentencepiece_length=128,
+    max_sentence_length=4192*4,
+    pad_id=0,
+    eos_id=1,
+    unk_id=2,
+    bos_id=-1,
+    required_chars=required_chars,
+)
+ 
+ 
+print("Addining missing tokens to NLLB tokenizer and saving result")
+# reading the NLLB and the Tyvan sentencepiece models into a native format
+tokenizer = NllbTokenizer.from_pretrained('facebook/nllb-200-distilled-600M')
+sp_trained = spm.SentencePieceProcessor(model_file=f'{SPM_PREFIX}.model')
+added_spm = sp_pb2_model.ModelProto()
+added_spm.ParseFromString(sp_trained.serialized_model_proto())
+old_spm = sp_pb2_model.ModelProto()
+old_spm.ParseFromString(tokenizer.sp_model.serialized_model_proto())
+ 
+# adding the missing tokens to the NLLB sentencepiece model
+nllb_tokens_set = {p.piece for p in old_spm.pieces}
+prev_min_score = old_spm.pieces[-1].score
+for p in added_spm.pieces:
+    piece = p.piece
+    # !!! THIS FIX WAS ADDED LATER; it is required for CT2 compatibility !!!
+    # 1 is ordinary token, non-1 is special token; we don't want to copy the special tokens
+    if p.type != 1:
+        continue
+    if piece not in nllb_tokens_set:
+        new_p = sp_pb2_model.ModelProto().SentencePiece()
+        new_p.piece = piece
+        # for all new tokens, I'll set a lower score (priority)
+        new_p.score = p.score + prev_min_score
+        old_spm.pieces.append(new_p)
+ 
+# saving the result to disk
+NEW_SPM_NAME = 'spm_nllb_mansi_268k.model'
+with open(NEW_SPM_NAME, 'wb') as f:
+    f.write(old_spm.SerializeToString())
+ 
+ 
+# loading the tokenizers
+print("Reloading default NLLB tokenizer to ours and resizing model")
+model_name = 'facebook/nllb-200-distilled-600M'
+tokenizer_old = NllbTokenizer.from_pretrained(model_name)
+tokenizer = NllbTokenizer.from_pretrained(model_name, vocab_file=NEW_SPM_NAME)
+print(len(tokenizer_old), len(tokenizer)) # 256204, 268559
+added_vocab = set(tokenizer.get_vocab()).difference(set(tokenizer_old.get_vocab()))
+print(len(added_vocab))  # 12355
+ 
+# loading and resizing the model
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+model.resize_token_embeddings(len(tokenizer))
+ 
+# re-initializing the new embeddings
+for t in tqdm(added_vocab):
+    tt = tokenizer_old(t, add_special_tokens=False).input_ids
+    if len(tt) == 0:
+        tt = [tokenizer_old.unk_token_id]
+    idx = tokenizer.convert_tokens_to_ids(t)
+    model.model.shared.weight.data[idx] = model.model.shared.weight.data[tt].mean(0)
