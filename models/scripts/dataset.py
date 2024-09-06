f709ccb0893a512f5db4154ae8ff1661c710c706
Rustam Akimov <raki@1c.ru>
2024-09-06 15:11:56 +0300

    update train, add dataset, still raw

--- models/scripts/dataset.py
+++ models/scripts/dataset.py
@@ -1,9 +1,116 @@
-import json
-from pathlib import Path
-
+import unicodedata
 import pandas as pd
-import torch
-from torch.nn.utils.rnn import pad_sequence
+import re
+import typing as tp
+import sys
 from torch.utils.data import Dataset
+import random
+from sacremoses import MosesPunctNormalizer
+from transformers import NllbTokenizer
 
-# TODO
+# TODO: Refactor this bulshit
+mpn = MosesPunctNormalizer(lang="en")
+mpn.substitutions = [
+    (re.compile(r), sub) for r, sub in mpn.substitutions
+]
+
+def get_non_printing_char_replacer(replace_by: str = " ") -> tp.Callable[[str], str]:
+    non_printable_map = {
+        ord(c): replace_by
+        for c in (chr(i) for i in range(sys.maxunicode + 1))
+        # same as \p{C} in perl
+        # see https://www.unicode.org/reports/tr44/#General_Category_Values
+        if unicodedata.category(c) in {"C", "Cc", "Cf", "Cs", "Co", "Cn"}
+    }
+
+    def replace_non_printing_char(line) -> str:
+        return line.translate(non_printable_map)
+
+    return replace_non_printing_char
+
+replace_nonprint = get_non_printing_char_replacer(" ")
+
+
+def preproc(text):
+    clean = mpn.normalize(text)
+    clean = replace_nonprint(clean)
+    # replace 𝓕𝔯𝔞𝔫𝔠𝔢𝔰𝔠𝔞 by Francesca
+    clean = unicodedata.normalize("NFKC", clean)
+    return clean
+
+
+LANGS = [('ru', 'rus_Cyrl'), ('mansi', 'mansi_Cyrl')]
+
+
+
+
+def load_data(dataset_path):
+    # TODO: Add split save ???
+    
+    df = pd.read_csv(dataset_path)
+
+    df = df.rename(columns={ # TODO: This is bulshit, remove it
+        'target': 'mansi',
+        'source': 'ru'
+    })
+
+    shuffled_df = df.sample(frac=1).reset_index(drop=True)
+
+    total_size = len(shuffled_df)
+    test_size = int(total_size * 0.1)
+    val_size = int(total_size * 0.1)
+    # train_size = total_size - test_size - val_size
+
+    test_df = shuffled_df[:test_size]
+    val_df = shuffled_df[test_size: test_size + val_size]
+    train_df = shuffled_df[test_size + val_size:]
+
+
+    print(f"Train size: {len(train_df)}")
+    print(f"Val size: {len(val_df)}")
+    print(f"Test size: {len(test_df)}")
+
+    return train_df, test_df, val_df
+
+
+
+
+class MansiRusDataset(Dataset):
+    def __init__(self, df):
+        self.df = df
+
+    def __getitem__(self, idx):
+        item = self.df.iloc[idx]
+
+        return item
+    
+    def __len__(self):
+        return len(self.df)
+
+class CollateFn():
+    def __init__(self, tokenizer: NllbTokenizer, ignore_index = -100) -> None:
+        self.tokenizer = tokenizer
+        self.ignore_index = ignore_index # NOTE: -100 is default ignore_index
+
+    def __call__(self, batch: list) -> dict:
+        return self.pad_batch(batch)
+
+    def pad_batch(self, batch: list) -> dict:
+        (l1, lang1), (l2, lang2) = random.sample(LANGS, 2) # Random choice between [ru->mansi, mansi->ru]
+        
+        x_texts, y_texts = [], []
+        for item in batch:
+            x_texts.append(preproc(item[l1]))
+            y_texts.append(preproc(item[l2]))
+
+        self.tokenizer.src_lang = lang1
+        x = self.tokenizer(x_texts, return_tensors='pt', padding='longest')
+        self.tokenizer.src_lang = lang2
+        y = self.tokenizer(y_texts, return_tensors='pt', padding='longest')
+
+        y.input_ids[y.input_ids == self.tokenizer.pad_token_id] = self.ignore_index
+
+        return {
+            "x": x,
+            "y": y,
+        }
--- models/scripts/train.py
+++ models/scripts/train.py
@@ -1,50 +1,50 @@
+from pathlib import Path
 import pytorch_lightning as pl
 import torch
 import typer
-import torch.nn as nn
 from typing import Optional
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
-from torch.optim import AdamW
-from transformers import  get_cosine_schedule_with_warmup, AutoModelForSeq2SeqLM
-
+from pytorch_lightning.utilities import rank_zero_only
+# from pytorch_lightning.strategies import FSDPStrategy
+from transformers.optimization import Adafactor
+from transformers import  get_constant_schedule_with_warmup, AutoModelForSeq2SeqLM, NllbTokenizer
+from tqdm import tqdm
+from dataset import load_data, CollateFn, MansiRusDataset
+from torch.utils.data import DataLoader
 
 torch.set_float32_matmul_precision('medium')
 
+# TODO: Think about model loading by config
 
 
 class LightningModel(pl.LightningModule):
-    def __init__(self) -> None:
+    def __init__(self, model: AutoModelForSeq2SeqLM, tokenizer: NllbTokenizer) -> None:
         super(LightningModel, self).__init__()
-        self.model = AutoModelForSeq2SeqLM.from_pretrained('facebook/nllb-200-distilled-600M')
-
-        self.criterion = nn.CrossEntropyLoss(ignore_index = None)
+        self.model = model
+        self.tokenizer = tokenizer
 
         self.test_metrics = []
 
-    def forward(self, tokens):
-        out = self.model.forward(tokens)
+    def forward(self, src):
+        out = self.model(**src)
 
         return out
 
-    def training_step(self, batch, batch_idx):
-        tokens, target = batch.values()
-
-        out = self.model.forward(tokens)
+    def training_step(self, batch):
+        src, tgt = batch.values()
         
-        loss = self.criterion(out.view(-1, ), target.reshape(-1))
+        loss = self.model(**src, labels=tgt.input_ids).loss
         self.log("train_loss", loss, prog_bar=True)
 
         return loss
         
     def validation_step(self, batch, batch_idx):
-        tokens, target = batch.values()
-
-        out = self.model.forward(tokens)
+        src, tgt = batch.values()
         
-        loss = self.criterion(out.view(-1, ), target.reshape(-1))
-        self.log("val_loss", loss, prog_bar=True, sync_dist=True)
+        loss = self.model(**src, labels=tgt.input_ids).loss
+        self.log("val_loss", loss, prog_bar=True)
 
         return loss
     
@@ -54,12 +54,16 @@ class LightningModel(pl.LightningModule):
     def on_test_epoch_end(self):
         pass
 
-        
-    
     def configure_optimizers(self):
-        optimizer = AdamW(self.model.parameters(), lr = 2e-4, betas = (0.9, 0.95))
-        # scheduler = CosineAnnealingLR(optimizer, self.trainer.max_steps)
-        scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=1000, num_training_steps=self.trainer.max_steps)
+        optimizer = Adafactor(
+            [p for p in self.model.parameters() if p.requires_grad],
+            scale_parameter=False,
+            relative_step=False,
+            lr=1e-4,
+            clip_threshold=1.0,
+            weight_decay=1e-3,
+        )
+        scheduler = get_constant_schedule_with_warmup(optimizer, num_warmup_steps=1_000)
 
         return {
             'optimizer': optimizer,
@@ -71,7 +75,37 @@ class LightningModel(pl.LightningModule):
         }
     
 
-def train(batch_size: int = 32, config_path: str = "model_config.json", checkpoints_dir: str = "models/checkpoint", checkpoint_path: Optional[str] = None,  model_id: str = "facebook/nllb-200-distilled-600M"):
+@rank_zero_only
+def prepare_model_and_tokenizer(model_name, vocab_file):
+    # TODO: Refactor this
+
+    # loading the tokenizers
+    tokenizer_old = NllbTokenizer.from_pretrained(model_name)
+    tokenizer = NllbTokenizer.from_pretrained(model_name, vocab_file=vocab_file)
+
+    added_vocab = set(tokenizer.get_vocab()).difference(set(tokenizer_old.get_vocab()))
+
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    model.resize_token_embeddings(len(tokenizer))
+
+    # re-initializing the new embeddings
+    for t in tqdm(added_vocab, desc = "Re-initializing new embeddings"):
+        tt = tokenizer_old(t, add_special_tokens=False).input_ids
+        if len(tt) == 0:
+            tt = [tokenizer_old.unk_token_id]
+        idx = tokenizer.convert_tokens_to_ids(t)
+        model.model.shared.weight.data[idx] = model.model.shared.weight.data[tt].mean(0)
+
+    model.save_pretrained("re-init/model/nllb-200-distilled-600M")
+    tokenizer.save_pretrained("re-init/tokenizer/nllb-200-distilled-600M")
+
+    del model, tokenizer
+    
+
+def train(batch_size: int = 16, checkpoints_dir: str = "models/checkpoint", checkpoint_path: Optional[str] = None,  model_name: str = "facebook/nllb-200-distilled-600M", vocab_file: str = "spm_nllb_mansi_268k.model"):
+
+    train_df, test_df, val_df = load_data("data/src/rus_mansi_overall_80K.csv")
+    
     logger = TensorBoardLogger("./tb_logs")
 
     checkpoint_callback = ModelCheckpoint(
@@ -83,17 +117,28 @@ def train(batch_size: int = 32, config_path: str = "model_config.json", checkpoi
         save_last=True
     )
 
-    model_config = None # TODO
-    train_dataloader = None # TODO
-    val_dataloader = None # TODO
+    init_dir: Path = Path("re-init")
+    if not init_dir.exists():
+        init_dir.mkdir(parents=True, exist_ok=True)
+        prepare_model_and_tokenizer(model_name, vocab_file)
+        
+    model = AutoModelForSeq2SeqLM.from_pretrained("re-init/model/nllb-200-distilled-600M")
+    tokenizer = NllbTokenizer.from_pretrained("re-init/tokenizer/nllb-200-distilled-600M")
+
+    train_dataset = MansiRusDataset(train_df)
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=CollateFn(tokenizer), num_workers=14)
+
+    val_dataset = MansiRusDataset(val_df)
+    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, collate_fn=CollateFn(tokenizer), num_workers=14)
 
     lr_monitor = LearningRateMonitor(logging_interval='step')
-    lightning_model = LightningModel(model_config)
+    lightning_model = LightningModel(model, tokenizer)
 
-    trainer = Trainer(max_steps=500000, callbacks=[checkpoint_callback, lr_monitor], strategy='ddp', logger=logger, devices = "auto", log_every_n_steps=1, val_check_interval=4482, precision="16-mixed") # check_val_every_n_epoch=1 val_check_interval=4482,
+    trainer = Trainer(max_steps=500000, callbacks=[checkpoint_callback, lr_monitor], strategy="ddp", logger=logger, devices = "auto", log_every_n_steps=1, check_val_every_n_epoch = 1, precision="16-mixed") # check_val_every_n_epoch=1 val_check_interval=4482,
     trainer.fit(model=lightning_model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader, ckpt_path=checkpoint_path)
 
 
 
 if __name__ == "__main__":
     typer.run(train)
+    
